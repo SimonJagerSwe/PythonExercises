810e2171fa0bb6fcc9c4119289331d9f53b5e853
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-31 01:47:13 +0200

    easier than expected

--- exes_and_os.py
+++ exes_and_os.py
@@ -1,3 +1,20 @@
 ########## X's & O's ##########
 string = str(input('Input a string: '))
 upper_string = string.upper()
+
+def x_o_counter():
+    xs = []
+    os = []
+    for char in upper_string:
+        if char == 'X':
+            xs.append(char)
+        if char == 'O':
+            os.append(char)
+    # print(xs)
+    # print(os)
+    if len(xs) == len(os):
+        return True
+    else:
+        return False
+
+print(x_o_counter())
