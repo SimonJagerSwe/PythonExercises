2402506e64aa5d5c0e8f336e8507cd4c925a3542
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-30 02:19:42 +0200

    actually understood the problem

--- decimal_to_binary.py
+++ decimal_to_binary.py
@@ -1,14 +1,13 @@
 ########## DECIMAL TO BINARY ##########
 
-dec_num = float(input('Enter a floating point number of 1024 or lower: '))
+dec_num = int(input('Enter a floating point number of 1024 or lower: '))
 
 def bin_conversion():
-    float(bin_num = bin(dec_num))
-    print(dec_num)
+    bin_num = bin(dec_num)
+    print(bin_num)
 
 if dec_num > 1024:
     print('Float too great! Exiting program')
 
 else:
     bin_conversion()
-
