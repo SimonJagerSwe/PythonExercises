22c6b8fc170f6aec43a6140348e2dce4c7793325
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-31 01:52:02 +0200

    file and functions setup

---
+++ calculator.py
@@ -0,0 +1,17 @@
+########## CALCULATOR ##########
+int_a = int(input('Enter first integer: '))
+operator = str(input('Enter operator: '))
+int_b = int(input('Enter second integer: '))
+
+def addition():
+    return int_a + int_b
+
+def subtraction():
+    return int_a - int_b
+
+def multiplication():
+    return int_a * int_b
+
+def division():
+    return int_a / int_b
+
