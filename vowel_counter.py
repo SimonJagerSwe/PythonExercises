886718cb9ba910f98dbbce7bbed6a86dcee253b9
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-30 16:41:37 +0200

    using len() instead of count() works

--- vowel_counter.py
+++ vowel_counter.py
@@ -1,3 +1,13 @@
 ########## VOWEL COUNTER ##########
 
 str = str(input('Please input a string to be counted: '))
+vowel_list = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u']
+
+def vowel_counter():
+    vowels_in_string = []
+    for char in str:
+        if char in vowel_list:
+            vowels_in_string.append(char)
+    print(len(vowels_in_string))
+
+vowel_counter()
