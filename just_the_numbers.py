9f6473eda12d9c48b29d7c2eaa43951b0baa8146
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-31 02:16:48 +0200

    a bit tricky

--- just_the_numbers.py
+++ just_the_numbers.py
@@ -1,2 +1,12 @@
 ########## JUST THE NUMBERS ##########
 mixed_list = ['My name is Simon and I\'m 34, I mean Thirtyfour, Years old', 34, 'ABCD', 1, 35, '0', 'CLANGGG', 11]
+
+def just_numbers():
+    int_list = []
+    for num in mixed_list:
+        if num is int == True:
+            int_list.append(num)
+    print(int_list)
+
+# print(mixed_list)
+just_numbers()
