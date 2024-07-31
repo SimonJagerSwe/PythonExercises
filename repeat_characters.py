2060d50678171d0e2476e7961c7e5733508f98c9
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-31 16:41:26 +0200

    found .join method and solved the problem

--- repeat_characters.py
+++ repeat_characters.py
@@ -2,4 +2,11 @@
 single_string = str(input('Enter a string to be doubled: '))
 
 def string_doubling():
-    
+    double_string = []
+    for char in single_string:
+        double_string.append((char * 2))
+        double_string2 = ''.join(double_string)
+    print(double_string2)
+
+print(single_string)
+string_doubling()
