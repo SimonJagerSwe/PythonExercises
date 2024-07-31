db789dbf346eb8d127d4ad2b577150c7504c7b96
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-31 16:22:47 +0200

    found out about isinstance which solved the problem

--- just_the_numbers.py
+++ just_the_numbers.py
@@ -3,9 +3,9 @@ mixed_list = ['My name is Simon and I\'m 34, I mean Thirtyfour, Years old', 34, 
 
 def just_numbers():
     int_list = []
-    for num in mixed_list:
-        if num is int == True:
-            int_list.append(num)
+    for _ in mixed_list:
+        if isinstance(_, int):
+            int_list.append(_)
     print(int_list)
 
 # print(mixed_list)
