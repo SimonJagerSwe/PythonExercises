400198093f0956f2f05aab999c5c2851803d98ac
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-31 02:02:58 +0200

    problem solved

--- discount.py
+++ discount.py
@@ -1,4 +1,5 @@
 ########## DISCOUNT ##########
 price = float(input('Enter the price of the product: '))
 discount = float(input('Enter the discount in percent: '))
-discounted_price = price - (price * (discount / 10))
+discounted_price = price - (price * (discount / 100))
+print(discounted_price)
