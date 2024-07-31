2afab2f895d5f4e0a8f73e442dc5bfb1ad069d9d
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-31 02:01:19 +0200

    Started challenge

---
+++ discount.py
@@ -0,0 +1,4 @@
+########## DISCOUNT ##########
+price = float(input('Enter the price of the product: '))
+discount = float(input('Enter the discount in percent: '))
+discounted_price = price - (price * (discount / 10))
