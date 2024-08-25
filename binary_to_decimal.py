64d14ec9765b12bcddca032a19fd6c2a8ff10901
SimonJagerSwe <Simonjager@hotmail.com>
2024-08-25 15:22:21 +0200

    Eeeeh, sort of...

--- binary_to_decimal.py
+++ binary_to_decimal.py
@@ -1,2 +1,13 @@
 ########## CONVERT BINARY TO DECIMAL ##########
- 
+binary = bin(input(print('Enter a binary number: ')))
+
+def bin_to_dec(binary):
+    decimal, i = 0, 0
+    while(binary != 0):
+        dec = binary % 10
+        decimal = decimal + dec * pow(2, i)
+        binary = binary // 10
+        i += 1
+    return decimal
+
+print(bin_to_dec())
